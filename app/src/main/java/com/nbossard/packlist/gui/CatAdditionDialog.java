a940b8317248ed142472d57070f9d8359bc6a3d5
Nicolas Bossard <NicolasBossard@hotmail.com>
2016-02-12 23:04:51 +0100

    Initialisation of dialogs for adding categories to new trip

---
+++ app/src/main/java/com/nbossard/packlist/gui/CatAdditionDialog.java
@@ -0,0 +1,73 @@
+/*
+ * PackList is an open-source packing-list for Android
+ *
+ * Copyright (c) 2016 Nicolas Bossard.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.nbossard.packlist.gui;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.nbossard.packlist.R;
+
+/**
+ * @author Created by nbossard on 12/02/16.
+ */
+public class CatAdditionDialog extends DialogFragment {
+
+
+    /**
+     * Create a new instance of CatAdditionDialog, providing "num"
+     * as an argument.
+     */
+    static CatAdditionDialog newInstance(int num) {
+        CatAdditionDialog f = new CatAdditionDialog();
+
+        // Supply num input as an argument.
+        Bundle args = new Bundle();
+        args.putInt("num", num);
+        f.setArguments(args);
+
+        return f;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_dialog, container, false);
+        View tv = v.findViewById(R.id.text);
+        ((TextView)tv).setText("coucou");
+
+        // Watch for button clicks.
+        Button button = (Button)v.findViewById(R.id.show);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // When button is clicked, do someting
+
+            }
+        });
+
+        return v;
+    }
+
+}
