d984f64d36b114b27e05f362039341a85c36e423
Nicolas Bossard <NicolasBossard@hotmail.com>
2016-04-24 23:42:40 +0200

    initial commit for branch, first test running

---
+++ app/src/androidTest/java/com/nbossard/packlist/uiAutomator.java
@@ -0,0 +1,95 @@
+
+/*
+ * PackList is an open-source packing-list for Android
+ *
+ * Copyright (c) 2016 Nicolas Bossard and other contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.nbossard.packlist;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.test.filters.SdkSuppress;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+import android.support.test.uiautomator.Until;
+import android.support.test.InstrumentationRegistry;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+
+@RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 18)
+public class uiAutomator {
+
+    private static final String BASIC_SAMPLE_PACKAGE
+            = "com.nbossard.packlist.debug";
+    private static final int LAUNCH_TIMEOUT = 5000;
+    private static final String STRING_TO_BE_TYPED = "UiAutomator";
+    private UiDevice mDevice;
+
+    @Before
+    public void startMainActivityFromHomeScreen() {
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        // Start from the home screen
+        mDevice.pressHome();
+
+        // Wait for launcher
+        final String launcherPackage = mDevice.getLauncherPackageName();
+        assertThat(launcherPackage, notNullValue());
+        mDevice.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)),
+                LAUNCH_TIMEOUT);
+
+        // Launch the app
+        Context context = InstrumentationRegistry.getContext();
+        final Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(BASIC_SAMPLE_PACKAGE);
+        // Clear out any previous instances
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+
+        // Wait for the app to appear
+        mDevice.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),
+                LAUNCH_TIMEOUT);
+    }
+
+    @Test
+    public void testOpenAbout() throws UiObjectNotFoundException, InterruptedException {
+        UiObject menuButton = mDevice.findObject(new UiSelector().descriptionContains("More options"));
+
+        // Simulate a user-click on the menu button, if found.
+        if (menuButton.exists() && menuButton.isEnabled()) {
+            menuButton.click();
+        }
+
+        mDevice.wait(Until.findObject(By.text("About")), TestValues.LET_UI_THREAD_UPDATE_DISPLAY);
+
+        UiObject menuAbout = mDevice.findObject(new UiSelector().text("About"));
+        assertTrue(menuAbout.exists());
+    }
+
+}
---
+++ doc/tests.md
@@ -0,0 +1,3 @@
+
+http://developer.android.com/tools/testing-support-library/index.html#UIAutomator
+http://developer.android.com/training/testing/ui-testing/uiautomator-testing.html
