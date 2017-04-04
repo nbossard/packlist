05c94adbc0c6f6e2f6d675eb72ea089fc18fb695
Nicolas Bossard <NicolasBossard@hotmail.com>
2017-04-04 23:50:52 +0200

    Adding opening of settings activity

---
+++ app/src/main/groovy/com/nbossard/packlist/gui/SettingsFragment.java
@@ -0,0 +1,50 @@
+/*
+ * PackList is an open-source packing-list for Android
+ *
+ * Copyright (c) 2017 Nicolas Bossard and other contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.nbossard.packlist.gui;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import com.nbossard.packlist.R;
+/*
+@startuml
+    class com.nbossard.packlist.gui.SettingsFragment {
+    }
+
+@enduml
+ */
+
+/**
+ * Fragment for displaying settings, classical implementation.
+ *
+ * @author Created by nbossard on 03/04/17.
+ */
+
+public class SettingsFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+}
